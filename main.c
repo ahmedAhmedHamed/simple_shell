6e6b43d83b4aaf7b83740d9785824031c700deba
vboxuser <vboxuser@ubuntu22.04.1.myguest.virtualbox.org>
2023-04-14 19:58:17 +0200

    ground work with working /bin/ls

---
+++ main.c
@@ -0,0 +1,50 @@
+//
+// Created by vboxuser on 14/04/23.
+//
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include "main.h"
+
+/**
+ * main - hello
+ * Return: exit code
+ */
+int main(void)
+{
+	int waitID;
+	while (true)
+	{
+		char buffer[1000];
+		char *b = buffer;
+		size_t bufSize = 1000;
+		size_t characters;
+		int processID;
+		printf("$ ");
+		characters = getline(&b, &bufSize,  stdin);
+		if (characters == 1)
+			return (0);
+		processID = fork();
+		if (!processID)
+		{
+			struct stat i;
+			b[characters - 1] = '\0';
+			extern char **environ;
+			if (stat(b, &i))
+			{
+				fprintf(stderr, "No such file or directory\n");
+				continue;
+			}
+			char *argv[] = {b, 0};
+			printf("%s", b);
+			execve(b, &argv, environ);
+			perror("execve");
+			return (0);
+		}
+		wait(&waitID);
+	}
+	return (0);
+}
---
+++ main.h
@@ -0,0 +1,8 @@
+//
+// Created by vboxuser on 14/04/23.
+//
+
+#ifndef SIMPLE_SHELL_MAIN_H
+#define SIMPLE_SHELL_MAIN_H
+
+#endif //SIMPLE_SHELL_MAIN_H
