771f7822694e83c1c026e1c7eb96968f1e8e85ed
vboxuser <vboxuser@ubuntu22.04.1.myguest.virtualbox.org>
2023-04-26 10:34:25 +0200

    initial test with argv and _argv, this should be broken

--- main.c
+++ main.c
@@ -1,10 +1,20 @@
 #include "main.h"
 
+void move_argv_into__argv(char *argv[], char *_argv[10])
+{
+	int i = 0;
+	while (argv[i] != 0)
+	{
+		_argv[i] = argv[i];
+		i++;
+	}
+}
+
 /**
  * main - simple shell
  * Return: exit code
  */
-int main(void)
+int main(int argc, char *argv[])
 {
 	int waitID;
 	char buffer[BUFSIZE];
@@ -12,24 +22,25 @@ int main(void)
 	size_t characters;
 	int processID;
 	struct stat istat;
-	char *argv[10];/*TODO change if too low*/
+	char *_argv[10];/*TODO change if too low*/
 
 	while (true)
 	{
-		characters = setupInput(argv, b);
+		characters = setupInput(_argv, b);
+		move_argv_into__argv(argv, _argv);
 		if (feof(stdin)) /*checking for end of file*/
 			return (0);
-		formatString(characters, argv, b);
-		if (!strcmp(argv[0], "exit"))
+		formatString(characters, _argv, b);
+		if (!strcmp(_argv[0], "exit"))
 		{
-			if (argv[1] != NULL)
-				return (atoi(argv[1]));
+			if (_argv[1] != NULL)
+				return (atoi(_argv[1]));
 			else
 				return (0);
 		}
-		if (checkFunctions(argv))
+		if (checkFunctions(_argv))
 			continue;
-		if (stat(argv[0], &istat))/*checking if file exists*/
+		if (stat(_argv[0], &istat))/*checking if file exists*/
 		{
 			fprintf(stderr, "No such file or directory\n");
 			continue;
@@ -37,7 +48,7 @@ int main(void)
 		processID = fork();
 		if (!processID)/*evaluates to true in fork's child*/
 		{
-			execve(argv[0], argv, environ);
+			execve(_argv[0], _argv, environ);
 			perror("execve");
 			return (0);
 		}
