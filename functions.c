353c03248150f69e77e44f9a7dfc0a02efb0f39b
vboxuser <vboxuser@ubuntu22.04.1.myguest.virtualbox.org>
2023-04-26 19:45:22 +0200

    altered env

--- functions.c
+++ functions.c
@@ -9,8 +9,8 @@ void printenv(void)
 
 	for (i = 0; environ[i] != NULL; i++)
 	{
-		write(STDOUT_FILENO, "\n", 1);
 		write(STDOUT_FILENO, environ[i], _strlen(environ[i]));
+		write(STDOUT_FILENO, "\n", 1);
 	}
 	write(STDOUT_FILENO, "\n", 1);
 }
