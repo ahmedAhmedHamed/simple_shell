c518e49e42bdcee9a9025e2c8514624869d2a4dc
vboxuser <vboxuser@ubuntu22.04.1.myguest.virtualbox.org>
2023-04-26 08:42:31 +0200

    setup header

--- main.h
+++ main.h
@@ -1,8 +1,8 @@
-//
-// Created by vboxuser on 14/04/23.
-//
-
 #ifndef SIMPLE_SHELL_MAIN_H
 #define SIMPLE_SHELL_MAIN_H
-
-#endif //SIMPLE_SHELL_MAIN_H
+void _unsetenv(char *argv[10]);
+void _setenv(char *argv[10]);
+void strtoking(char *argv[10], const char *b);
+void printenv();
+int _strlen(const char *s);
+#endif
