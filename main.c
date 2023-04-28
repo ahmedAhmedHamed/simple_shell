1aea59ba06d974cfbdace1013db5ac4e01ac8ae0
AhmedHamed <AhmedAhmedHamedAhmed0@gmail.com>
2023-04-28 15:11:09 +0300

    went back to et3amel

--- main.c
+++ main.c
@@ -14,75 +14,23 @@ void SignalHandler(int sig)
 }
 
 /**
- * myStrCpy - copies from to to using malloc WITHOUT any protection
- * @from: string to be copied
- * @to: string to be copied to
- */
-void myStrCpy(char *from, char **to)
-{
-	int i = 0;
-	int stop = _strlen(from);
-	(*to) = malloc(_strlen(from) * sizeof(char));
-
-	while (i <= stop)
-	{
-		(*to)[i] = from[i];
-		i++;
-	}
-}
-
-/**
- *
- */
-void handlePipeInput(char *argv[], char *envp[])
-{
-	int processID;
-	struct stat istat;
-
-	processID = fork();
-	if (!processID)/*evaluates to true in fork's child*/
-	{
-		if (stat(argv[0], &istat))/*checking if file exists*/
-		{/*error message likely needs to be changed*/
-			write(1, argv[0], _strlen(argv[0]));
-			write(STDERR_FILENO, ": No such file or directory\n", 28);
-			return;
-		}
-		execve(argv[0], argv, envp);
-		perror("execve");
-	}
-	wait(&processID);
-}
-
-/**
  * argv - ...
  * @argv: ...
  * Return: ...
  */
-int pipedInputCase(char *envp[])
+int et3amel(char *argv[], char *envp[])
 {
-	int characters;
-	int waitID;
-	int i = 4;
-	char *b = NULL;/*necessary for usage with getline*/
-	char *nextArgv[10];
+	struct stat istat;
 
-	while (i--)
-	{
-		signal(SIGINT, SignalHandler);
-		characters = setupInput(nextArgv, &b);
-		if (characters == -1)
-			return (0);
-		formatString(characters, nextArgv, b);
-		handlePipeInput(nextArgv, envp);
-
-		wait(&waitID);
-		frees(nextArgv);
-		free(b);
-		b = NULL;
-
+	if (stat(argv[0], &istat))/*checking if file exists*/
+	{/*error message likely needs to be changed*/
+		write(1, argv[0], _strlen(argv[0]));
+		write(STDERR_FILENO, ": No such file or directorya7a\n", 28);
+		return (0);
 	}
-	return (0);
+	execve(argv[0], argv, envp);
+	perror("execve");
+	return (1);
 }
 
 /**
@@ -101,9 +49,6 @@ int main(int argc, char *argv[], char *envp[])
 	(void) argc;
 	(void) argv;
 
-	if (!isatty(STDIN_FILENO))
-		return (pipedInputCase(envp));/*might be wrong return value*/
-
 	while (true)
 	{
 		signal(SIGINT, SignalHandler);
@@ -118,8 +63,12 @@ int main(int argc, char *argv[], char *envp[])
 		if (isEqual(nextArgv[0], "env"))
 		{
 			printenv(envp);
+			if (!isatty(STDIN_FILENO))
+				return (0);
 			continue;
 		}
+		if (!isatty(STDIN_FILENO))
+			return (et3amel(nextArgv, envp));/*might be wrong return value*/
 
 		if (fileExists(nextArgv))
 			continue;
